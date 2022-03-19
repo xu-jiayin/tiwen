86669016e5e56a79d0f09b3a9c772af5ee186b1c
xu-jiayin <xjy30681@gmail.com>
2022-03-19 16:36:43 +0800

    1

---
+++ config.py
@@ -0,0 +1,11 @@
+# 提温填报API
+url = 'http://xg.fjsdxy.com/SPCP/Web/Temperature/StuTemperatureInfo'
+#pushplus推送加API
+pushurl = 'http://www.pushplus.plus/send'
+# pushplus推送加token
+token = ''
+# 通知标题
+title= '体温填报情况'
+# headers信息
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"}
+msg = ''
